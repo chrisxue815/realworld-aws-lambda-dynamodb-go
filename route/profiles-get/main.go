59f317e01bc645f28c42f26d59fdca57e537e966
Chris Xue <chrisxue815@gmail.com>
2019-07-18 23:44:07 +0100

    Get profiles

---
+++ route/profiles-get/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/chrisxue815/realworld-aws-lambda-dynamodb-go/service"
+	"github.com/chrisxue815/realworld-aws-lambda-dynamodb-go/util"
+)
+
+type ResponseBody struct {
+	Username  string `json:"username"`
+	Image     string `json:"image"`
+	Bio       string `json:"bio"`
+	Following bool   `json:"following"`
+}
+
+func Handle(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	user, _, _ := service.GetCurrentUser(request.Headers["Authorization"])
+
+	profile, err := service.GetUserByUsername(request.PathParameters["username"])
+	if err != nil {
+		return util.NewErrorResponse(err)
+	}
+
+	following, err := service.IsFollowing(user, []string{profile.Username})
+	if err != nil {
+		return util.NewErrorResponse(err)
+	}
+
+	responseBody := ResponseBody{
+		Username:  profile.Username,
+		Image:     profile.Image,
+		Bio:       profile.Bio,
+		Following: following[0],
+	}
+
+	return util.NewSuccessResponse(200, responseBody)
+}
+
+func main() {
+	lambda.Start(Handle)
+}
