a7313df925a1014ddc113dc4c26b8570be426c94
Chris Xue <chrisxue815@gmail.com>
2019-09-02 15:32:54 +0100

    Benchmark passing article by value and by pointer

--- model/Article_test.go
+++ model/Article_test.go
@@ -21,3 +21,37 @@ func TestSlugToArticleId(t *testing.T) {
 		assert.Equal(t, testCase.expectedError, err != nil, "%+v", testCase)
 	}
 }
+
+func PassArticleByValue(article Article, goPanic bool) {
+	if goPanic {
+		if article.ArticleId == 0 {
+			// noinline
+			// https://github.com/golang/go/wiki/CompilerOptimizations#function-inlining
+			panic(nil)
+		}
+	}
+}
+
+func PassArticleByPointer(article *Article, goPanic bool) {
+	if goPanic {
+		if article.ArticleId == 0 {
+			// noinline
+			// https://github.com/golang/go/wiki/CompilerOptimizations#function-inlining
+			panic(nil)
+		}
+	}
+}
+
+func BenchmarkPassArticleByValue(b *testing.B) {
+	article := Article{}
+	for i := 0; i < b.N; i++ {
+		PassArticleByValue(article, false)
+	}
+}
+
+func BenchmarkPassArticleByPointer(b *testing.B) {
+	article := Article{}
+	for i := 0; i < b.N; i++ {
+		PassArticleByPointer(&article, false)
+	}
+}
